9d6d922dabe90144d0db8b8a36b2a1c33896fc73
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-10-07 20:45:38 -0400

    Add project files.

---
+++ GeneralStore.MVC/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "# In Stock")]
+        public int InventoryCount { get; set; }
+        public decimal Price { get; set; }
+        [Required]
+        [Display(Name ="Is It Food")]
+        public bool IsFood { get; set; }
+
+    }
+}
